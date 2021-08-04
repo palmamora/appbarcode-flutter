9b32f6b15fdf9908aeff9f73202778ff43ad3b8c
Manuel Palma Mora <m.palma12@ufromail.cl>
2021-08-04 03:34:19 -0400

    lista reserva works

--- lib/pages/lista_reservas_page.dart
+++ lib/pages/lista_reservas_page.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -10,63 +9,58 @@ class ListaReservasPage extends StatefulWidget {
 }
 
 class _ListaReservasPageState extends State<ListaReservasPage> {
-  List<Widget> _widgets = [];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista Reservas'),
       ),
-      body: ListView(
-        children: _widgets,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _widgets = _refreshWidgets();
-
-          });
+      body: FutureBuilder<List<String>>(
+        future: _getCodes(),
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(),
+                Text(
+                  '\n   Cargando...',
+                  style: TextStyle(color: Colors.red),
+                )
+              ],
+            ));
+          } else {
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index]),
+                  );
+                });
+          }
         },
-        child: const Icon(Icons.refresh),
       ),
     );
   }
 
   Future<List<String>> _getCodes() async {
-    List<String> l = [];
+    List<String> lst = [];
     var url = Uri.parse('http://palma.6te.net/api.php');
     var res = await http.post(url,
         body: jsonEncode(<String, String>{
           'tipo': 'lista_reservas',
           'data': '',
         }));
-    print(res.statusCode);
     Map<dynamic, dynamic> reqCodes = stringToMap(res.body);
-    reqCodes.forEach((key, value) {
-      l.add(value);
+    reqCodes.forEach((k, v) {
+      lst.add(v.toString());
     });
-    print(l);
-    return l;
+    return lst;
   }
 
   Map<dynamic, dynamic> stringToMap(String s) {
     Map<dynamic, dynamic> map = json.decode(s);
     return map;
-  }
-
-  List<Widget> _refreshWidgets() {
-    List<Widget> l = [];
-    _getCodes().then((List<String> list) {
-      for (var i = 0; i < list.length; i++) {
-        l
-          ..add(ListTile(
-            title: Text(list[i]),
-          ))
-          ..add(Divider());
-      }
-      return l;
-    });
-    return l;
   }
 }
